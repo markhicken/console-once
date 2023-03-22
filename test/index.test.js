4693ceb59616f80a88203139c958a65166663829
Mark Hicken <mark.hicken@gmail.com>
2023-03-22 09:49:39 -0600

    simplify tests

--- test/index.test.js
+++ test/index.test.js
@@ -3,6 +3,11 @@ const {debug, info, log, warn, error} = consoleOnce;
 
 function assertTestsByFn(fn) {
   const fnName = fn['fnName'] || fn.name;
+  const spyFnName = fnName === 'consoleOnce' ? 'log' : fnName;
+  // if a console shortcut function is used, the
+  // consoleOnce function signature is different!
+  const baseArgs = fnName === 'consoleOnce' ? [spyFnName, 'testKey'] : ['testKey'];
+
   describe(fnName, () => {
     const console = global?.console || window?.console;
     afterEach(()=>{
@@ -11,41 +16,22 @@ function assertTestsByFn(fn) {
     });
 
     test('calls the function only once', () => {
-      if (fnName === 'consoleOnce') {
-        jest.spyOn(console, 'log');
-        fn('log', 'testKey', 'test');
-        fn('log', 'testKey', 'test2');
-        expect(console['log']).toHaveBeenCalledTimes(1);
-      } else {
-        jest.spyOn(console, fnName);
-        fn('testKey', 'test');
-        fn('testKey', 'test2');
-        expect(console[fnName]).toHaveBeenCalledTimes(1);
-      }
+      jest.spyOn(console, spyFnName);
+      fn(...[...baseArgs, 'test']);
+      fn(...[...baseArgs, 'test2']);
+      expect(console[spyFnName]).toHaveBeenCalledTimes(1);
     });
 
     test('calls the expected function with 1 argument', () => {
-      fnName === 'consoleOnce' ?
-        jest.spyOn(console, 'log'):
-        jest.spyOn(console, fnName);
-      fnName === 'consoleOnce' ?
-        fn('log', 'testKey', 'test'):
-        fn('testKey', 'test');
-      fnName === 'consoleOnce' ?
-        expect(console['log']).toHaveBeenCalledWith('test'):
-        expect(console[fnName]).toHaveBeenCalledWith('test');
+      jest.spyOn(console, spyFnName);
+      fn(...[...baseArgs, 'test']);
+      expect(console[spyFnName]).toHaveBeenCalledWith('test');
     });
 
     test('calls the expected function with more than 1 argument', () => {
-      fnName === 'consoleOnce' ?
-        jest.spyOn(console, 'log'):
-        jest.spyOn(console, fnName);
-      fnName === 'consoleOnce' ?
-        fn('log', 'testKey', 'test', 'test2', 'test3'):
-        fn('testKey', 'test', 'test2', 'test3');
-      fnName === 'consoleOnce' ?
-        expect(console['log']).toHaveBeenCalledWith('test', 'test2', 'test3'):
-        expect(console[fnName]).toHaveBeenCalledWith('test', 'test2', 'test3');
+      jest.spyOn(console, spyFnName);
+      fn(...[...baseArgs, 'test', 'test2', 'test3']);
+      expect(console[spyFnName]).toHaveBeenCalledWith('test', 'test2', 'test3');
     });
   });
 }
